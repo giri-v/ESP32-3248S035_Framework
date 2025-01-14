ef8ec9494e31b9db052a695c562d8540c92d59cc
giri-v <gvenkat_00@yahoo.com>
2025-01-13 20:49:09 -0800

    Added JPG support

--- src/framework.h
+++ src/framework.h
@@ -46,6 +46,7 @@ extern "C"
 #include <TFT_eSPI.h>
 #include <OpenFontRender.h>
 #include <PNGdec.h>
+#include <JPEGDecoder.h>
 #define MAX_IMAGE_WIDTH 320
 
 #include <HTTPClient.h>
@@ -86,10 +87,13 @@ MqttStream mqttStream = MqttStream(&client);
 char topic[128] = "log/foo";
 #endif
 
-#include "AudioFileSourceSD.h"
-#include "AudioFileSourceID3.h"
-#include "AudioGeneratorMP3.h"
-#include "AudioOutputI2S.h"
+#define minimum(a, b) (((a) < (b)) ? (a) : (b))
+#define maximum(a, b) (((a) > (b)) ? (a) : (b))
+
+#include <AudioFileSourceSD.h>
+#include <AudioFileSourceID3.h>
+#include <AudioGeneratorMP3.h>
+#include <AudioOutputI2S.h>
 
 AudioGeneratorMP3 *mp3;
 AudioOutputI2S *out;
@@ -101,8 +105,10 @@ int screenWidth = tft.width();
 int screenHeight = tft.height();
 int screenCenterX = tft.width() / 2;
 int screenCenterY = tft.height() / 2;
+PNG png;
+int32_t xPos = 0;
+int32_t yPos = 0;
 
-int chip_id = ESP.getEfuseMac();
 
 #ifdef APP_NAME
 const char *appName = APP_NAME;
@@ -157,41 +163,10 @@ char latestFirmwareFileName[100];
 // **************** Debug Parameters ************************
 String methodName = "";
 
-// Here are the callback functions that the decPNG library
-// will use to open files, fetch data and close the file.
-
+// PNGDec Veriables
 File pngfile;
 
-void *pngOpen(const char *filename, int32_t *size)
-{
-    Log.verboseln("Attempting to open %s\n", filename);
-    pngfile = SPIFFS.open(filename, "r");
-    *size = pngfile.size();
-    return &pngfile;
-}
 
-void pngClose(void *handle)
-{
-    File pngfile = *((File *)handle);
-    if (pngfile)
-        pngfile.close();
-}
-
-int32_t pngRead(PNGFILE *page, uint8_t *buffer, int32_t length)
-{
-    if (!pngfile)
-        return 0;
-    page = page; // Avoid warning
-    return pngfile.read(buffer, length);
-}
-
-int32_t pngSeek(PNGFILE *page, int32_t position)
-{
-    if (!pngfile)
-        return 0;
-    page = page; // Avoid warning
-    return pngfile.seek(position);
-}
 
 bool isNullorEmpty(char *str)
 {
@@ -259,6 +234,40 @@ void playMP3Loop()
     }
 }
 
+// PNGDec File I/O Helper Functions
+void *pngOpen(const char *filename, int32_t *size)
+{
+    Log.verboseln("Attempting to open %s\n", filename);
+    pngfile = SPIFFS.open(filename, "r");
+    *size = pngfile.size();
+    return &pngfile;
+}
+
+void pngClose(void *handle)
+{
+    File pngfile = *((File *)handle);
+    if (pngfile)
+        pngfile.close();
+}
+
+int32_t pngRead(PNGFILE *page, uint8_t *buffer, int32_t length)
+{
+    if (!pngfile)
+        return 0;
+    page = page; // Avoid warning
+    return pngfile.read(buffer, length);
+}
+
+int32_t pngSeek(PNGFILE *page, int32_t position)
+{
+    if (!pngfile)
+        return 0;
+    page = page; // Avoid warning
+    return pngfile.seek(position);
+}
+
+/// @brief Draw the PNG Image 
+/// @param pDraw 
 void pngDraw(PNGDRAW *pDraw)
 {
     uint16_t lineBuffer[MAX_IMAGE_WIDTH];
@@ -289,6 +298,179 @@ void drawPNG(const char *filename, int x, int y)
             png.close();
         }
         tft.endWrite();
+    }
+}
+
+// ####################################################################################################
+//  Draw a JPEG on the TFT, images will be cropped on the right/bottom sides if they do not fit
+// ####################################################################################################
+//  This function assumes xpos,ypos is a valid screen coordinate. For convenience images that do not
+//  fit totally on the screen are cropped to the nearest MCU size and may leave right/bottom borders.
+void renderJPEG(int xpos, int ypos)
+{
+
+    // retrieve information about the image
+    uint16_t *pImg;
+    uint16_t mcu_w = JpegDec.MCUWidth;
+    uint16_t mcu_h = JpegDec.MCUHeight;
+    uint32_t max_x = JpegDec.width;
+    uint32_t max_y = JpegDec.height;
+
+    // Jpeg images are draw as a set of image block (tiles) called Minimum Coding Units (MCUs)
+    // Typically these MCUs are 16x16 pixel blocks
+    // Determine the width and height of the right and bottom edge image blocks
+    uint32_t min_w = minimum(mcu_w, max_x % mcu_w);
+    uint32_t min_h = minimum(mcu_h, max_y % mcu_h);
+
+    // save the current image block size
+    uint32_t win_w = mcu_w;
+    uint32_t win_h = mcu_h;
+
+    // record the current time so we can measure how long it takes to draw an image
+    uint32_t drawTime = millis();
+
+    // save the coordinate of the right and bottom edges to assist image cropping
+    // to the screen size
+    max_x += xpos;
+    max_y += ypos;
+
+    // read each MCU block until there are no more
+    while (JpegDec.read())
+    {
+
+        // save a pointer to the image block
+        pImg = JpegDec.pImage;
+
+        // calculate where the image block should be drawn on the screen
+        int mcu_x = JpegDec.MCUx * mcu_w + xpos; // Calculate coordinates of top left corner of current MCU
+        int mcu_y = JpegDec.MCUy * mcu_h + ypos;
+
+        // check if the image block size needs to be changed for the right edge
+        if (mcu_x + mcu_w <= max_x)
+            win_w = mcu_w;
+        else
+            win_w = min_w;
+
+        // check if the image block size needs to be changed for the bottom edge
+        if (mcu_y + mcu_h <= max_y)
+            win_h = mcu_h;
+        else
+            win_h = min_h;
+
+        // copy pixels into a contiguous block
+        if (win_w != mcu_w)
+        {
+            uint16_t *cImg;
+            int p = 0;
+            cImg = pImg + win_w;
+            for (int h = 1; h < win_h; h++)
+            {
+                p += mcu_w;
+                for (int w = 0; w < win_w; w++)
+                {
+                    *cImg = *(pImg + w + p);
+                    cImg++;
+                }
+            }
+        }
+
+        // calculate how many pixels must be drawn
+        uint32_t mcu_pixels = win_w * win_h;
+
+        tft.startWrite();
+
+        // draw image MCU block only if it will fit on the screen
+        if ((mcu_x + win_w) <= tft.width() && (mcu_y + win_h) <= tft.height())
+        {
+
+            // Now set a MCU bounding window on the TFT to push pixels into (x, y, x + width - 1, y + height - 1)
+            tft.setAddrWindow(mcu_x, mcu_y, win_w, win_h);
+
+            // Write all MCU pixels to the TFT window
+            while (mcu_pixels--)
+            {
+                // Push each pixel to the TFT MCU area
+                tft.pushColor(*pImg++);
+            }
+        }
+        else if ((mcu_y + win_h) >= tft.height())
+            JpegDec.abort(); // Image has run off bottom of screen so abort decoding
+
+        tft.endWrite();
+    }
+
+    // calculate how long it took to draw the image
+    drawTime = millis() - drawTime;
+
+    // print the results to the serial port
+    // Log.infoln("renderJPEG(): Total render time was: %ims", drawTime);
+}
+
+// ####################################################################################################
+//  Print image information to the serial port (optional)
+// ####################################################################################################
+void jpegInfo()
+{
+    Log.infoln(F("==============="));
+    Log.infoln(F("JPEG image info"));
+    Log.infoln(F("==============="));
+    Log.info(F("Width      :"));
+    Log.infoln("%i", JpegDec.width);
+    Log.info(F("Height     :"));
+    Log.infoln("%i", JpegDec.height);
+    Log.info(F("Components :"));
+    Log.infoln("%i", JpegDec.comps);
+    Log.info(F("MCU / row  :"));
+    Log.infoln("%i", JpegDec.MCUSPerRow);
+    Log.info(F("MCU / col  :"));
+    Log.infoln("%i", JpegDec.MCUSPerCol);
+    // Log.info(F("Scan type  :"));
+    // Log.infoln(JpegDec.scanType);
+    Log.info(F("MCU width  :"));
+    Log.infoln("%i", JpegDec.MCUWidth);
+    Log.info(F("MCU height :"));
+    Log.infoln("%i", JpegDec.MCUHeight);
+    Log.infoln(F("==============="));
+}
+
+// ####################################################################################################
+//  Show the execution time (optional)
+// ####################################################################################################
+//  WARNING: for UNO/AVR legacy reasons printing text to the screen with the Mega might not work for
+//  sketch sizes greater than ~70KBytes because 16-bit address pointers are used in some libraries.
+
+// The Due will work fine with the HX8357_Due library.
+
+void showTime(uint32_t msTime)
+{
+    // tft.setCursor(0, 0);
+    // tft.setTextFont(1);
+    // tft.setTextSize(2);
+    // tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    // tft.print(F(" JPEG drawn in "));
+    // tft.print(msTime);
+    // tft.println(F(" ms "));
+    Log.info(F(" JPEG drawn in "));
+    Log.info("%i", msTime);
+    Log.infoln(F(" ms "));
+}
+
+void drawArrayJpeg(const uint8_t arrayname[], uint32_t array_size, int xpos, int ypos)
+{
+
+    boolean decoded = JpegDec.decodeArray(arrayname, array_size);
+
+    if (decoded)
+    {
+        // print information about the image to the serial port
+        jpegInfo();
+
+        // render the image onto the screen at given coordinates
+        renderJPEG(xpos, ypos);
+    }
+    else
+    {
+        Serial.println("Jpeg file format not supported!");
     }
 }
 
